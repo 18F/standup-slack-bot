dd773af0a3c1769bb8c2ac7d8940d7a13b392852
stvnrlly <steven.reilly@gsa.gov>
2016-02-17 11:28:39 -0500

    add ability to set channel standup schedule

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 'use strict';
 
 var Botkit = require('botkit');
-var dateFormat = require('dateformat');
+var moment = require('moment');
 require('dotenv').config();
+
+// Database setup
+var models = require('./models');
+models.sequelize.sync(
+  // Set to true to reset db on load
+  {force: false}
+);
 
 if (!process.env.SLACK_TOKEN) {
   console.log('Error: Specify token in environment');
   process.exit(1);
 }
 
-// Database
-var models = require('./models');
-models.sequelize.sync(
-  {force: true}
-);
-
 var controller = Botkit.slackbot({
- debug: false
+  debug: false
 });
 
+var me = '';
 controller.spawn({
   token: process.env.SLACK_TOKEN
-}).startRTM(function(err) {
+}).startRTM(function(err, bot) {
   if (err) {
     throw new Error(err);
+  } else {
+    bot.identifyBot(function(err,identity) {
+      me = identity.name;
+      // identity contains...
+      // {name, id, team_id}
+    });
   }
 });
 
@@ -32,8 +40,50 @@ controller.spawn({
 // TODO: add usage messages
 // TODO: remind people to do standup?
 
+controller.on('bot_channel_join', function (bot, message) {
+  return bot.reply(message, {
+    text: 'Hi! To set up a standup, say `@'+me+' create standup [daily/weekly] [time]`'
+  });
+});
+
+controller.hears(['(schedule|create) standup (.*)'],['direct_mention'], function (bot, message) {
+  // fields = message.match[2].split(' ');
+  var frequency = '';
+  var weekday = '';
+  var time = message.match[2].match(/(\d+|:)*(\s)?((a|p)m)/gi);
+  time = moment(time[0], ['h:mm a','hmm a','hmma','HHmm','hha','hh a']);
+  if (message.match[2].match(/daily/)) {
+    frequency = 'daily';
+  } else if (message.match[2].match(/weekly/)) {
+    frequency = 'weekly';
+    weekday = message.match[2].match(/(\w)*day/i);
+  }
+  models.Channel.findOrCreate({
+    where: {
+      name: message.channel
+    }
+  }).then(function () {
+    console.log('channel created');
+    console.log(time);
+    models.Channel.update(
+      {
+        frequency: frequency,
+        day: weekday,
+        time: moment(time).format('HHmm')
+      },
+      {
+        where: {
+          name: message.channel
+        }
+      }
+    );
+    return bot.reply(message,
+      'Got it. Standup scheduled '+frequency+' at '+moment(time).format('hh:mm a')+' '+weekday
+    );
+  });
+});
+
 // Add or change a standup message in a DM with the bot
-// TODO: store standups in db
 // TODO: allow multiple ways to separate messages (i.e. \n or ; or |)
 // TODO: update reports when edited
 // TODO: parse standup messages
@@ -43,11 +93,11 @@ controller.hears(['standup (\\S*)((.|\n)*)'],['direct_message'], function (bot, 
   models.Channel.findOne({
     where: {name: standupChannel}
   }).then(function (channel) {
-    if (!channel) {
+    if (channel) {
       models.Standup.findOrCreate({
         where: {
           channel: standupChannel,
-          date: dateFormat(new Date(), 'isoDate'),
+          date: moment().format('YYYY-MM-DD'),
           user: message.user
         }
       }).then(function (standup) {
@@ -61,19 +111,19 @@ controller.hears(['standup (\\S*)((.|\n)*)'],['direct_message'], function (bot, 
           var item = notes[note].replace(/\n/g,'');
           switch (item[0]) {
             case 'y':
-              yesterday = item;
-              break;
+            yesterday = item;
+            break;
             case 't':
-              today = item;
-              break;
+            today = item;
+            break;
             case 'b':
-              blockers = item;
-              break;
+            blockers = item;
+            break;
             case 'g':
-              goal = item;
-              break;
+            goal = item;
+            break;
             default:
-              console.log('no match for '+item);
+            console.log('no match for '+item);
           }
         }
         models.Standup.update(
@@ -83,16 +133,20 @@ controller.hears(['standup (\\S*)((.|\n)*)'],['direct_message'], function (bot, 
             blockers: blockers,
             goal: goal
           },
-          {where: {
-            channel: standupChannel,
-            date: dateFormat(new Date(), 'isoDate'),
-            user: message.user
-          }
-        });
-      });
+          {
+            where: {
+              channel: standupChannel,
+              date: moment().format('YYYY-MM-DD'),
+              user: message.user
+            }
+          });
+        }
+      );
       return bot.reply(message, 'that channel exists');
     } else {
-      return bot.reply(message, 'The '+standupChannel+' channel doesn\'t have any standups set');
+      return bot.reply(message,
+        'The '+standupChannel+' channel doesn\'t have any standups set'
+      );
     }
   });
 });
