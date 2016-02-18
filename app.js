393a01260ad2054ed629518c1d7d748d0b060fff
Greg Walker <michael.walker@gsa.gov>
2016-02-18 14:48:34 -0600

    • Add another module • Rename modules to prefix them with functional units (we can discuss this before merging) • Moved actual bot setup to inside the identifyBot callback, so everything is available locally; this also mitigates a situation where the identifyBot function fails but all the handlers are hooked up anywhere.  This is a pretty minor situation (the join-channel message is incomplete) so I'm not set on this at all.

--- app.js
+++ app.js
@@ -21,11 +21,6 @@ var controller = Botkit.slackbot({
   debug: false
 });
 
-// Set these two variables so that we can access bot functions outside of a controller
-// function, for example when calling bot.say() without a message trigger
-var me = '';
-var anytimeBot = {};
-
 // Initialize the bot
 controller.spawn({
   token: process.env.SLACK_TOKEN
@@ -33,36 +28,32 @@ controller.spawn({
   if (err) {
     throw new Error(err);
   } else {
-    anytimeBot = bot;
+    //anytimeBot = bot;
     bot.identifyBot(function(err,identity) {
-      me = identity.name;
+      //me = identity.name;
       // identity contains...
       // {name, id, team_id}
+
+      // Set up cron job to check every minute for channels that need a standup report
+      schedule.scheduleJob('* * * * *', require('./lib/runReports')(bot));
+
+      // TODO: method to set standup frequency
+      // TODO: add usage messages
+      // TODO: remind people to do standup?
+
+      // Message for when the bot is added to a channel
+      require('./lib/bot-joinChannel')(controller, identity.name);
+
+      // Create a standup in a channel
+      require('./lib/createStandup')(controller);
+
+      // Add or change a standup message for today in a DM with the bot
+      require('./lib/getUserStandupInfo')(controller);
+
+      // I think that these aren't necessary because channel & user are stored as
+      // unique id rather than display name
+      // TODO: update channel name if it changes
+      // TODO: update user name if it changes
     });
   }
 });
-
-// Set up cron job to check every minute for channels that need a standup report
-schedule.scheduleJob('* * * * *', require('./lib/runReports')(anytimeBot));
-
-// TODO: method to set standup frequency
-// TODO: add usage messages
-// TODO: remind people to do standup?
-
-// Message for when the bot is added to a channel
-controller.on('bot_channel_join', function (bot, message) {
-  return bot.reply(message, {
-    text: 'Hi! To set up a standup, say `@'+me+' create standup [daily/weekly] [time]`'
-  });
-});
-
-// Create a standup in a channel
-require('./lib/createStandup')(controller);
-
-// Add or change a standup message for today in a DM with the bot
-require('./lib/getUserStandupInfo')(controller);
-
-// I think that these aren't necessary because channel & user are stored as
-// unique id rather than display name
-// TODO: update channel name if it changes
-// TODO: update user name if it changes
