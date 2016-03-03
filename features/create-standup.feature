90bfd9d9d784e0f05dbdd68fb6ad0a5cf050d8c3
Greg Walker <michael.walker@gsa.gov>
2016-03-03 11:39:14 -0600

    Increase coverage

--- features/create-standup.feature
+++ features/create-standup.feature
@@ -4,7 +4,18 @@ Feature: Create standup meetings on a recurring basis
     Given the bot is running
     And I am in a room with the bot
     When I say "@bot create standup at <time>"
-    Then the bot should respond "Got it"
+    Then the bot should respond "<response-start>"
+
+    Examples:
+      | time | response-start                            |
+      | 0830 | Got it                                    |
+      | 130  | Sorry, I couldn't understand that message |
+
+  Scenario Outline: I try to create a standup
+    Given the bot is running
+    And I am in a private room with the bot
+    When I say "@bot create standup at <time>"
+    Then the bot should respond "I can only work with public channels"
 
     Examples:
       | time |
--- features/steps/create-standup.js
+++ features/steps/create-standup.js
@@ -14,6 +14,12 @@ module.exports = function() {
     _message.channel = 'CSomethingSaySomething';
   });
 
+  this.Given('I am in a private room with the bot', function() {
+    botLib.createStandup(common.botController);
+    _createStandupFn = common.getHandler(common.botController.hears);
+    _message.channel = 'PnutButterJellyTime';
+  });
+
   this.When(/I say "@bot ((create|schedule) (standup .*))"/,
     function(message, triggerWord, rest, done) {
       _message.type = 'message';
@@ -35,11 +41,12 @@ module.exports = function() {
       });
   });
 
-  this.Then('the bot should respond "Got it"', function() {
-    if(/^Got it/.test(_botReply)) {
+  this.Then(/the bot should respond "([^"]+)"/, function(responseStart) {
+    if(_botReply.startsWith(responseStart)) {
       return true;
     } else {
-      throw new Error('Bot reply did not start with "Got it"');
+      console.log(_botReply)
+      throw new Error('Bot reply did not start with "' + responseStart + '"');
     }
   });
 };
