b069a19316de04427456a19cfe825997300e268d
stvnrlly <steven.reilly@gsa.gov>
2016-04-13 17:05:36 -0400

    add requirements for sequelize migrations

---
+++ migrations/20160413161355-test-migration.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.changeColumn(
+      'Standups',
+      'yesterday',
+      {
+        type: Sequelize.STRING(1000)
+      }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
