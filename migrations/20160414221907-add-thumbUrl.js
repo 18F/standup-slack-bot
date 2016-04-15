9574adaf911ba66c2ecaa6fd10e3538b5fc5b96d
stvnrlly <steven.reilly@gsa.gov>
2016-04-14 23:51:17 -0400

    add thumbUrl for standup report

---
+++ migrations/20160414221907-add-thumbUrl.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn(
+      'Standups',
+      'thumbUrl',
+      {
+        type: Sequelize.STRING
+      }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
