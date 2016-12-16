5c7bb0ac4f728c8feb99d9abc8415731e3d215dc
Greg Walker <michael.walker@gsa.gov>
2016-12-16 13:53:40 -0600

    Update dependencies; shrinkwrap dependencies before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,5 +42,8 @@ else
   exit 1
 fi
 
+# Shrinkwrap dependencies to the versions that were actually tested
+npm shrinkwrap
+
 cf login -a $API -u $CF_USERNAME -p $CF_PASSWORD -o $ORG -s standup-bot
 cf zero-downtime-push $NAME -f $MANIFEST
