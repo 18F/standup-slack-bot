0d77af24fb4cf73ab142785b8351a6ecd7984d81
Greg Walker <mgwalker@gmail.com>
2017-09-22 20:22:20 -0500

    add postUpdatesInChannel config; don't update channel with new reports if config isn't set

---
+++ migrations/04-add-channel-update-config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn(
+      'Channels',
+      'postUpdatesInChannel',
+      {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      }
+    );
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
