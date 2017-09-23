f66d8c133ed527111208aafc76a5aae5c78303e7
Greg Walker <mgwalker@gmail.com>
2017-09-22 20:56:09 -0500

    tests for the new config

---
+++ features/set-updates.feature
@@ -0,0 +1,13 @@
+Feature: Set in-channel report updates
+
+  Scenario Outline: I try to toggle in-channel report updates
+    Given the bot is running
+    And I am in a room with the bot
+    And the standup is scheduled for <utc-time>
+    When I say "@bot <toggle> updates"
+    Then the bot should respond "Okay, I <will-or-wont> update the channel with late reports"
+
+    Examples:
+      | utc-time | toggle  | will-or-wont  |
+      | 9:30 am  | enable  | will          |
+      | 10:30 am | disable | won't         |
---
+++ features/steps/set-updates.js
@@ -0,0 +1,20 @@
+'use strict';
+var botLib = require('../../lib/bot');
+var common = require('./common');
+
+module.exports = function() {
+  var _message = { };
+
+  this.When(/I say "@bot ((enable|disable) updates)"/,
+    function(message, which, done) {
+      botLib.setInChannelUpdate(common.botController);
+
+      _message.type = 'message';
+      _message.text = message;
+      _message.channel = _message.channel || 'CSomethingSaySomething';
+      console.log(`sending: ${message}`)
+
+      common.botRepliesToHearing(_message, done);
+  });
+
+};
